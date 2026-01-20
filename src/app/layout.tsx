20051e82e92440b2eddc78474022f5ae789b7b4b
Sckab <giulianodeamicis2009@gmail.com>
2026-01-20 17:27:54 +0100

    added scripts and formatted the whole site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,55 @@
-import type { Metadata } from "next";
-import "@/styles/globals.css";
-import Dropdown from "@/components/ThemeDropdown/Dropdown";
-import { ThemeProvider } from "next-themes";
-import Image from "next/image";
+import type { Metadata } from 'next'
+import '@/styles/globals.css'
+import Image from 'next/image'
+import { ThemeProvider } from 'next-themes'
+import Dropdown from '@/components/ThemeDropdown/Dropdown'
 
 export const metadata: Metadata = {
   title: {
-    default: "Create Mastery",
-    template: "%s | Create Mastery",
+    default: 'Create Mastery',
+    template: '%s | Create Mastery',
   },
-  description: "The official Create Mastery website",
-};
+  description: 'The official Create Mastery website',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className="flex min-h-screen flex-col">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <header className="flex flex-row justify-between bg-blue-300 p-2 text-stone-900 select-none">
-            <div className="flex flex-row items-center justify-center gap-2">
+    <html
+      lang='en'
+      suppressHydrationWarning
+    >
+      <body className='flex min-h-screen flex-col'>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+        >
+          <header className='flex flex-row justify-between bg-blue-300 p-2 text-stone-900 select-none'>
+            <div className='flex flex-row items-center justify-center gap-2'>
               <Image
-                src="/create-mastery-logo.svg"
-                alt="Create Mastery Logo"
+                src='/create-mastery-logo.svg'
+                alt='Create Mastery Logo'
                 width={70}
                 height={70}
               />
 
-              <span className="text-5xl font-bold">CREATE MASTERY</span>
+              <span className='text-5xl font-bold'>CREATE MASTERY</span>
             </div>
 
-            <div className="mr-4 flex items-center justify-center">
+            <div className='mr-4 flex items-center justify-center'>
               <Dropdown />
             </div>
           </header>
 
-          <main className="flex-1 bg-stone-100 dark:bg-stone-900">
+          <main className='flex-1 bg-stone-100 dark:bg-stone-900'>
             {children}
           </main>
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
--- src/components/ThemeDropdown/Dropdown.tsx
+++ src/components/ThemeDropdown/Dropdown.tsx
@@ -1,62 +1,77 @@
-"use client";
+'use client'
 
-import { DropdownMenu } from "radix-ui";
-import { useTheme } from "next-themes";
-import styles from "./styles.module.css";
-import { useState } from "react";
-import { Icon } from "@iconify/react";
+import { Icon } from '@iconify/react'
+import { useTheme } from 'next-themes'
+import { DropdownMenu } from 'radix-ui'
+import { useState } from 'react'
+import styles from './styles.module.css'
 
 const Dropdown = () => {
-  const [rotate, setOpen] = useState(false);
-  const { setTheme } = useTheme();
+  const [rotate, setOpen] = useState(false)
+  const { setTheme } = useTheme()
 
   return (
     <DropdownMenu.Root onOpenChange={setOpen}>
       <DropdownMenu.Trigger asChild>
         <button
           className={styles.themeDropdown__button}
-          aria-label="Theme Dropdown"
-          type="button"
+          aria-label='Theme Dropdown'
+          type='button'
         >
           THEME
           <Icon
-            icon="lucide:chevron-right"
-            width="32"
-            height="32"
+            icon='lucide:chevron-right'
+            width='32'
+            height='32'
             className={
               rotate
                 ? `${styles.themeDropdown__button__icon__rotate} transition-all`
-                : "transition-all"
+                : 'transition-all'
             }
           />
         </button>
       </DropdownMenu.Trigger>
 
-      <DropdownMenu.Content className="rounded-md bg-blue-300 p-2" loop>
+      <DropdownMenu.Content
+        className='rounded-md bg-blue-300 p-2'
+        loop
+      >
         <DropdownMenu.Item
           className={styles.themeDropdown__item}
-          onClick={() => setTheme("dark")}
+          onClick={() => setTheme('dark')}
         >
           DARK
-          <Icon icon="lucide:moon" width="24" height="24" />
+          <Icon
+            icon='lucide:moon'
+            width='24'
+            height='24'
+          />
         </DropdownMenu.Item>
         <DropdownMenu.Item
           className={styles.themeDropdown__item}
-          onClick={() => setTheme("light")}
+          onClick={() => setTheme('light')}
         >
           LIGHT
-          <Icon icon="lucide:sun-medium" width="24" height="24" />
+          <Icon
+            icon='lucide:sun-medium'
+            width='24'
+            height='24'
+          />
         </DropdownMenu.Item>
         <DropdownMenu.Item
           className={styles.themeDropdown__item}
-          onClick={() => setTheme("system")}
+          onClick={() => setTheme('system')}
         >
           SYSTEM
-          <Icon icon="lucide:monitor" width="24" height="24" />
+          <Icon
+            icon='lucide:monitor'
+            width='24'
+            height='24'
+          />
         </DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
-  );
-};
+  )
+}
 
-export default Dropdown;
+export default Dropdown
