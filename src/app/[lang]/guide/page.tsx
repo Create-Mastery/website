65d626b0fd70d3b83f95f35c26e2d528b426738b
Sckab <giulianodeamicis2009@gmail.com>
2026-01-25 00:35:36 +0100

    added template to the index of the guide

--- src/app/[lang]/guide/page.tsx
+++ src/app/[lang]/guide/page.tsx
@@ -1,3 +1,13 @@
-export default function page() {
+import { Locale } from '@/i18n/config'
+import { getDictionary } from '@/i18n/get-dictionaries'
+
+export default async function page(props: {
+  params: Promise<{
+    lang: Locale
+  }>
+}) {
+  const { lang } = await props.params
+  const dictionary = await getDictionary(lang)
+
   return <div></div>
 }
